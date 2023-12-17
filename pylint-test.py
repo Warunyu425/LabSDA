aeeabe4292f0b3514219620f0933a67f10d44ccf
Warunyu425 <154065986+Warunyu425@users.noreply.github.com>
2023-12-17 20:58:14 +0700

    Create pylint-test.py

---
+++ pylint-test.py
@@ -0,0 +1,25 @@
+# pylint_test.py
+
+"""
+Module documentation for pylint_test.
+"""
+
+import os
+import subprocess
+
+def run_pylint():
+    """
+    Run Pylint on all Python files in the current directory.
+    """
+    python_files = [file for file in os.listdir() if file.endswith('.py')]
+
+    if not python_files:
+        print("No Python files found.")
+        return
+
+    for python_file in python_files:
+        file_path = os.path.join(os.getcwd(), python_file)
+        subprocess.run(['pylint', file_path], check=True)
+
+if _name_ == "_main_":
+    run_pylint()
